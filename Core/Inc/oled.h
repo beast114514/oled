4edec153778b4c1812a5fcdd5291d867fb74ae31
m13306658793@163.com <m13306658793@163.com>
2022-04-04 23:14:18 +0800

    first commit

---
+++ Core/Inc/oled.h
@@ -0,0 +1,56 @@
+/*
+ * oled.h
+ *
+ *  Created on: Jun 19, 2020
+ *      Author: guolunlun
+ */
+
+#ifndef INC_USER_OLED_H_
+#define INC_USER_OLED_H_
+#include "main.h"
+#include "stdlib.h"
+#define OLED_MODE 0
+#define SIZE 8
+#define XLevelL		0x00
+#define XLevelH		0x10
+#define Max_Column	128
+#define Max_Row		64
+#define	Brightness	0xFF
+#define X_WIDTH 	128
+#define Y_WIDTH 	64
+
+#define OLED_ADDRESS 0x78
+//设备地址
+typedef uint32_t  u32;
+typedef uint16_t u16;
+typedef uint8_t  u8;
+
+
+#define OLED_CMD  0	//写命令
+#define OLED_DATA 1	//写数据
+
+
+//OLED控制用函数
+void OLED_WR_Byte(unsigned dat,unsigned cmd);
+void OLED_Display_On(void);
+void OLED_Display_Off(void);
+void OLED_Init(void);
+void OLED_Clear(void);
+void OLED_DrawPoint(u8 x,u8 y,u8 t);
+void OLED_Fill(u8 x1,u8 y1,u8 x2,u8 y2,u8 dot);
+void OLED_ShowChar(u8 x,u8 y,u8 chr,u8 Char_Size);
+void OLED_ShowNum(u8 x,u8 y,u32 num,u8 len,u8 size);
+void OLED_ShowString(u8 x,u8 y, u8 *p,u8 Char_Size);
+void OLED_Set_Pos(unsigned char x, unsigned char y);
+void OLED_ShowCHinese(u8 x,u8 y,u8 no);
+void OLED_DrawBMP(unsigned char x0, unsigned char y0,unsigned char x1, unsigned char y1,unsigned char BMP[]);
+
+void fill_picture(unsigned char fill_Data);
+void Picture();
+
+void Write_IIC_Command(unsigned char IIC_Command);
+void Write_IIC_Data(unsigned char IIC_Data);
+void Write_IIC_Byte(unsigned char IIC_Byte);
+
+
+#endif /* INC_USER_OLED_H_ */
